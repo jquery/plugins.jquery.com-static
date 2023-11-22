bf6578f8c0f8877ebc3a533a71006dae44d7641a
Timmy Willison <timmywil@users.noreply.github.com>
2023-11-22 11:32:52 -0500

    feat(tags): generate tags pages

---
+++ _data/metadata.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'The jQuery Plugin Registry',
+  url: 'https://plugins.jquery.com/',
+  language: 'en',
+  description: 'Static archive of the old plugins.jquery.com site.',
+  author: {
+    name: 'Timmy Willison',
+    email: 'timmywil@users.noreply.github.com',
+    url: 'https://timmywil.com/'
+  }
+}
