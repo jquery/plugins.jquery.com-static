6c9665e66c0b2266eba566ff3bae8e7773e49b9b
Timmy Willison <timmywil@users.noreply.github.com>
2023-12-05 22:50:54 -0500

    feat(plugin): build out plugin pages

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -3,21 +3,30 @@ const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   mode: 'jit',
-  content: ['_site/**/*.html'],
+  content: ['./content/**/*.{njk,md}', './_includes/**/*.njk'],
   safelist: [],
   theme: {
     extend: {
+      backgroundImage: {
+        'dark-tiles': 'url(/img/dark-grey-tile.png)',
+        'sidebar-gradient':
+          'radial-gradient(ellipse at left, #f2f2f2, #ffffff 80%)',
+        'sidebar-header-gradient': 'linear-gradient(to right, #efefef, #f3f3f3)'
+      },
       boxShadow: {
         border: '#000000 0 -1px 0'
       },
       colors: {
         background: '#0769ad',
         blue: {
+          DEFAULT: '#305e91',
+          hover: '#3d76b8',
           light: '#8ccffa'
         },
         border: {
           DEFAULT: 'rgb(0,0,0,0.2)',
-          dark: '#333333'
+          dark: '#333333',
+          light: '#cccccc'
         },
         button: {
           DEFAULT: '#0f67a1',
@@ -38,11 +47,20 @@ module.exports = {
           visited: '#0f67a1',
           light: '#e6e6e6'
         },
+        list: {
+          DEFAULT: '#4d4d4d'
+        },
         'footer-text': {
           DEFAULT: '#aaaaaa',
           hover: '#e6e6e6'
         },
-        transparent: 'transparent'
+        transparent: 'transparent',
+        white: {
+          DEFAULT: '#ffffff',
+          dark: '#f2f2f2',
+          hover: '#e6e6e6',
+          active: '#cccccc'
+        }
       },
       fontFamily: {
         ...defaultTheme.fontFamily,
